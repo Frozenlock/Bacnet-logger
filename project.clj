83d6e00dee047cfbab818e123bdff89e66148453
Frozenlock <frozenlock@gmail.com>
2012-09-30 20:20:23 -0400

    change dependencies version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject bacnet-logger "1.0.0"
+(defproject bacnet-logger "1.0.1"
   :description "Automatic logger for a BACnet network"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-time "0.4.2"]
-                 [bacnet-scan-utils "1.0.2"]
+                 [bacnet-scan-utils "1.0.3"]
                  [org.clojars.frozenlock/gzip64 "1.0.0"]
                  [clj-http "0.4.1"]
                  [overtone/at-at "1.0.0"]]
