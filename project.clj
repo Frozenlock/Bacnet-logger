c786ab8bec95cb847c6490c47f6d805614871d1c
Frozenlock <frozenlock@gmail.com>
2012-11-28 17:59:43 -0500

    new longer timeout

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Automatic logger for a BACnet network"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.2"]
-                 [bacnet-scan-utils "1.0.8"]
+                 [bacnet-scan-utils "1.1.0"]
                  [org.clojars.frozenlock/gzip64 "1.0.0"]
                  [clj-http "0.4.1"]
                  [seesaw "1.4.2"]
                  [overtone/at-at "1.0.0"]]
   :plugins [[lein-getdown "0.0.1"]]
-  :getdown {:appbase "https://bacnethelp.com:8443/getdown/logger/"
+  :getdown {:appbase "https://bacnethelp.com/getdown/logger/"
             :allow-offline true}
   :main bacnet-logger.core)
